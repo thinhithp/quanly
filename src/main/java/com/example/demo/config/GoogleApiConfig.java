ae2b004d8056ac137145cc69d0310d038188bcd4
QUANG THINH <thinh.ithp@gmail.com>
2023-09-15 00:48:21 +0700

    commit create google api

---
+++ src/main/java/com/example/demo/config/GoogleApiConfig.java
@@ -0,0 +1,45 @@
+package com.example.demo.config;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.FileList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class GoogleApiConfig {
+
+    @Bean
+    public Drive googleDriveService(GoogleCredential googleCredential) throws GeneralSecurityException, IOException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        return new Drive.Builder(HTTP_TRANSPORT,
+                JacksonFactory.getDefaultInstance(), googleCredential)
+                .build();
+    }
+
+    @Bean
+    public GoogleCredential googleCredential() throws GeneralSecurityException, IOException {
+        HttpTransport httpTransport = new NetHttpTransport();
+        JacksonFactory jsonFactory = new JacksonFactory();
+
+        GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream("C:\\Users\\QUANG THINH\\Desktop\\work\\quan2-399017-2b3e79d1e06a.json"), httpTransport, jsonFactory)
+                .createScoped(Collections.singleton("https://www.googleapis.com/auth/drive"));
+
+        return credential;
+    }
+}
+
