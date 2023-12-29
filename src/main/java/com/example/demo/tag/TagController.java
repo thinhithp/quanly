880b4824a82932c6e534dff75136cb8a9d9f5431
thinhnq <Th1nh1993@>
2023-12-29 15:34:28 +0700

    sd

--- src/main/java/com/example/demo/tag/TagController.java
+++ src/main/java/com/example/demo/tag/TagController.java
@@ -4,11 +4,16 @@ import com.example.demo.global.constants.ApiConstants;
 import com.example.demo.tag.dto.request.TagCreateDto;
 import com.example.demo.tag.dto.request.TagUpdateDto;
 import com.example.demo.tag.entity.TagEntity;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.util.Optional;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(ApiConstants.BASE + ApiConstants.Tag.BASE)
@@ -18,7 +23,7 @@ public class TagController {
     private final TagService service;
 
     @PostMapping(ApiConstants.Action.INSERT)
-    public TagEntity insert(@RequestBody @Valid  TagCreateDto onjInput) {
+    public TagEntity insert(@RequestBody @Valid TagCreateDto onjInput) {
         return this.service.insert(onjInput);
     }
 
@@ -28,7 +33,7 @@ public class TagController {
     }
 
     @GetMapping(ApiConstants.Action.DETAIL)
-    public Optional<TagEntity> getAllSanPhamIds(@RequestParam Long id){
+    public Optional<TagEntity> getAllSanPhamIds(@RequestParam Long id) {
         return this.service.getAllSanPhamIds(id);
     }
 
